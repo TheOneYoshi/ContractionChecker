a0ba379585403885da6bf2c2a49434572538eaf3
TheOneYoshi <40876259+TheOneYoshi@users.noreply.github.com>
2022-05-19 23:57:58 -0400

    Initial commit

---
+++ src/ContractionChecker.java
@@ -0,0 +1,9 @@
+/**
+ * @author Kieran Joshi
+ * @version 1.0
+ *
+ * A program that locates contractions within a text file, used to notify the user of contractions in formal writing.
+ */
+public class ContractionChecker {
+    
+}
