e2a25cb58f1c26a3510107e525535aae735ac5eb
TheOneYoshi <40876259+TheOneYoshi@users.noreply.github.com>
2022-05-20 00:08:20 -0400

    Created text file that will be used, added a scanner for the file and exception handling

--- src/ContractionChecker.java
+++ src/ContractionChecker.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.*;
 /**
  * @author Kieran Joshi
  * @version 1.0
@@ -5,5 +7,13 @@
  * A program that locates contractions within a text file, used to notify the user of contractions in formal writing.
  */
 public class ContractionChecker {
-    
+    public static void main(String[] args) {
+        try{
+            File text = new File("textfile.txt");
+            Scanner scnr = new Scanner(text);
+        }
+        catch(FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
